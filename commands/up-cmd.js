3079962da10dba1af49635218d5497d6ce91382a
Aliaksei Krutsko <wonder4ik@gmail.com>
2024-10-10 10:01:07 +0300

    feat: implement up

---
+++ commands/up-cmd.js
@@ -0,0 +1,18 @@
+import { chdir } from 'node:process';
+
+import { MESSAGES } from '../constants/messages.js';
+
+export class UpCmd {
+  constructor(params) {
+    this.params = params;
+  }
+
+  execute() {
+    if (this.params.length) {
+      console.log(MESSAGES.INVALID_INPUT);
+      return;
+    }
+
+    chdir('..');
+  }
+}
