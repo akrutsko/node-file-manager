fc20ecfbe66d17c4f33f67c1511fe1d2d1df2960
Aliaksei Krutsko <wonder4ik@gmail.com>
2024-10-10 11:20:55 +0300

    feat: implement cd

---
+++ commands/cd-cmd.js
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path';
+import { chdir, cwd } from 'node:process';
+
+import { MESSAGES } from '../constants/messages.js';
+
+export class CdCmd {
+  constructor(params) {
+    this.params = params;
+  }
+
+  execute() {
+    if (this.params.length !== 1) {
+      console.log(MESSAGES.INVALID_INPUT);
+      return;
+    }
+
+    const path = resolve(cwd(), this.params[0]);
+    chdir(path);
+  }
+}
